093e3be253f555665c4301f6276b0a5e1b38804b
Cicero <150862765+cicerorph@users.noreply.github.com>
2023-12-05 16:32:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,85 @@
+// Import necessary modules
 const express = require('express');
-const scratchblocks = require('scratchblocks');
+const puppeteer = require('puppeteer');
 
+// Create an Express application
 const app = express();
-const port = 3000;
+const port = 3000; // You can use any port you prefer
 
-app.get('/generate/:q', (req, res) => {
-  // Get the parameter 'q' from the request
-  const query = req.params.q;
+app.get('/generate/:q', async (req, res) => {
+    const args = req.params.q.split(' ');
+    const query = `#?style=scratch3&script=${encodeURIComponent(args.join(' '))}`;
 
-  // Replace '\n' with a newline character
-  const formattedQuery = query.replace(/\\n/g, '\n');
+    // Launch Puppeteer browser
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-  // Use scratchblocks to generate the image
-  const svgCode = scratchblocks.parse(formattedQuery);
-  const svgBuffer = Buffer.from(svgCode);
+    // Navigate to the scratchblocks page
+    await page.goto(`https://scratchblocks.github.io/${query}`, {
+        waitUntil: ['domcontentloaded', 'load']
+    });
 
-  // Send the image as a response
-  res.set('Content-Type', 'image/svg+xml');
-  res.send(svgBuffer);
+    // Wait for the page to load (you may adjust the wait time)
+    await page.waitForTimeout(2000);
+
+    // Get the HTML content of the page
+    const html = await page.content();
+    const identifier = '<a id="export-svg" class="export-link" download="scratchblocks.svg" href="';
+
+    // Find the link to the SVG image
+    const linkStart = html.substring(html.indexOf(identifier) + identifier.length);
+    const fullLink = decodeURIComponent(linkStart.substring(0, linkStart.indexOf('"')));
+
+    // Open the data URL in a new page
+    const urlPage = await browser.newPage();
+    await urlPage.goto(fullLink, {
+        waitUntil: ['domcontentloaded', 'load']
+    });
+
+    // Close the original page
+    await page.close();
+
+    // Get width and height of the SVG image
+    let svgWidth = 100;
+    let svgHeight = 100;
+    (() => {
+        const a = fullLink.substring(85);
+        const b = a.substring(0, a.indexOf('"'));
+        svgWidth = Number(b) || 100;
+        if (svgWidth > 1920) svgWidth = 1920;
+    })();
+    (() => {
+        const a = fullLink.substring(fullLink.indexOf('height="') + 8);
+        const b = a.substring(0, a.indexOf('"'));
+        svgHeight = Number(b) || 100;
+        if (svgHeight > 1080) svgHeight = 1080;
+    })();
+
+    // Generate the image
+    const image = await urlPage.screenshot({
+        type: 'png',
+        omitBackground: true,
+        clip: {
+            x: 0,
+            y: 0,
+            width: svgWidth,
+            height: svgHeight,
+        },
+    });
+
+    // Close the new page
+    await urlPage.close();
+
+    // Close the browser
+    await browser.close();
+
+    // Send the generated image as a response
+    res.set('Content-Type', 'image/png');
+    res.send(image);
 });
 
-// Start the server
+
+// Start the Express server
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    console.log(`Server is running at http://localhost:${port}`);
 });
